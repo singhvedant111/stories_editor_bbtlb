f7f4da7133df0f33cb3717eaddf6e2f571b1d0ab
singhvedant111 <singhvedant111@gmail.com>
2024-04-20 00:02:15 +0530

    Fixed getStroke migration

--- lib/src/presentation/painting_view/widgets/sketcher.dart
+++ lib/src/presentation/painting_view/widgets/sketcher.dart
@@ -11,7 +11,7 @@ class Sketcher extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     Paint paint = Paint();
-    List<Point>? outlinePoints;
+    List<Offset>? outlinePoints;
 
     for (int i = 0; i < lines.length; ++i) {
       switch (lines[i].paintingType) {
@@ -24,7 +24,7 @@ class Sketcher extends CustomPainter {
               lines[i].points,
 
               /// line width
-              size: lines[i].size,
+              options: StrokeOptions(size: lines[i].size,
 
               /// line thin
               thinning: -0.1,
@@ -35,11 +35,9 @@ class Sketcher extends CustomPainter {
               /// on complete line
               isComplete: lines[i].isComplete,
               streamline: 1,
-              taperEnd: 0,
-              taperStart: 0,
-              capEnd: true,
+              
               simulatePressure: true,
-              capStart: true);
+              ));
           break;
         case PaintingType.marker:
           paint = Paint()
@@ -54,7 +52,7 @@ class Sketcher extends CustomPainter {
             lines[i].points,
 
             /// line width
-            size: lines[i].size,
+            options: StrokeOptions(size: lines[i].size,
 
             /// line thin
             thinning: -0.1,
@@ -65,10 +63,7 @@ class Sketcher extends CustomPainter {
             /// on complete line
             isComplete: lines[i].isComplete,
              simulatePressure: lines[i].simulatePressure,
-              taperStart: 0,
-              taperEnd: 0,
-              capStart: true,
-              capEnd: true
+             )
           );
           break;
         case PaintingType.neon:
@@ -89,7 +84,7 @@ class Sketcher extends CustomPainter {
               lines[i].points,
 
               /// line width
-              size: lines[i].size,
+              options: StrokeOptions(size: lines[i].size,
 
               /// line thin
               thinning: -0.1,
@@ -101,10 +96,7 @@ class Sketcher extends CustomPainter {
               isComplete: lines[i].isComplete,
               streamline: lines[i].streamline,
               simulatePressure: lines[i].simulatePressure,
-              taperStart: 0,
-              taperEnd: 0,
-              capStart: true,
-              capEnd: true);
+              ));
           break;
       }
 
@@ -115,16 +107,16 @@ class Sketcher extends CustomPainter {
       } else if (outlinePoints.length < 2) {
         /// If the path only has one line, draw a dot.
         path.addOval(Rect.fromCircle(
-            center: Offset(outlinePoints[0].x, outlinePoints[0].y), radius: 1));
+            center: Offset(outlinePoints[0].dx, outlinePoints[0].dy), radius: 1));
       } else {
         /// Otherwise, draw a line that connects each point with a curve.
-        path.moveTo(outlinePoints[0].x, outlinePoints[0].y);
+        path.moveTo(outlinePoints[0].dx, outlinePoints[0].dy);
 
         for (int i = 1; i < outlinePoints.length - 1; ++i) {
           final p0 = outlinePoints[i];
           final p1 = outlinePoints[i + 1];
           path.quadraticBezierTo(
-              p0.x, p0.y, (p0.x + p1.x) / 2, (p0.y + p1.y) / 2);
+              p0.dx, p0.dy, (p0.dx + p1.dx) / 2, (p0.dy + p1.dy) / 2);
         }
       }
 
